fe76265956ab6f81dadcd6b72e11172570f1fd6a
kittipith <141337866+kittipith@users.noreply.github.com>
2026-03-22 20:40:09 +0700

    setup

---
+++ krulunteer/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-[5rem] font-bold text-red-500 underline'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
