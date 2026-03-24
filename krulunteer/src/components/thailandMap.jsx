20ffee2228b20edb6ed4a1fd37c5760fab166324
Thebestttttt <148238395+Thebestttttt@users.noreply.github.com>
2026-03-25 03:08:14 +0700

    home

---
+++ krulunteer/src/components/thailandMap.jsx
@@ -0,0 +1,76 @@
+import { MapContainer, TileLayer, GeoJSON, Tooltip } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import thailand from "../data/thailandData.json";
+import schoolDatas from "../data/schoolData.json";
+import React, { use } from "react";
+
+function MapFixer() {
+    const map = useMap();
+    useEffect(() => {
+        setTimeout(() => { map.invalidateSize(); }, 500);
+    }, [map]);
+    return null;
+}
+export default function ThailandMap() {
+    const [isReady, setIsReady] = React.useState(false);
+
+    React.useEffect(() => {
+        setIsReady(true);
+    }, []);
+
+    const schoolData = schoolDatas;
+    const getColor = (count) => {
+    if (count > 10) return "#166534";
+    if (count > 5) return "#16a34a";
+    if (count > 0) return "#4ade80";
+    return "#ffffff"; 
+  };
+
+    const getProvinceColor = (feature) => {
+        const name = feature.properties.NAME_1 || feature.properties.name; 
+        const count = schoolData[name] || 0;
+
+        return {
+            fillColor: getColor(count),
+            weight: 1,
+            color: "#282828",
+            fillOpacity: 0.7,
+        };
+    };
+
+    const onEachFeature = (feature, layer) => {
+        const name = feature.properties.NAME_1 || feature.properties.name;
+        const count = schoolData[name] || 0;
+        layer.bindTooltip(`${name}: ${count} schools`, { permanent: false, direction: "top" });
+        layer.on({
+            mouseover: (e) => {
+                e.target.setStyle({
+                    weight: 2,
+                    color: "#000",
+                    fillOpacity: 0.9,
+                });
+                e.target.bringToFront();
+            },
+            mouseout: (e) => {
+                e.target.setStyle(getProvinceColor(feature));
+            }
+        
+        });
+    };
+    if (!isReady || !thailand) return null;
+
+    return (
+        <div className="w-full h-[600px] rounded-3xl overflow-hidden shadow-lg border border-gray-200">
+            <MapContainer center={[10.7, 100.5]} zoom={5}
+                className="w-full h-full"
+                scrollWheelZoom={false}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <GeoJSON data={thailand.features} style={getProvinceColor} onEachFeature={onEachFeature} />
+
+            </MapContainer>
+        </div>
+    );
+
+
+
+}
--- krulunteer/src/pages/Home.jsx
+++ krulunteer/src/pages/Home.jsx
@@ -1,10 +1,63 @@
 import '../index.css'
 import Header from '../components/Header'
+import ThailandMap from '../components/thailandMap.jsx'
+import { useNavigate } from 'react-router-dom';
 
 function Home(){
+    const navigate = useNavigate();
+
+    const handleFindSchool = () => {
+        navigate("/find");
+    }
+    const handlePostNeed = () => {        
+        navigate("/post");
+    }
     return(
         <>
             <Header />
+            <div className="bg-gray-100  flex items-center justify-center px-10 py-10 mt-20">
+                <div className="max-w-6xl w-full grid md:grid-cols-2 gap-10 items-center">
+
+                    <div>
+                        <p className="text-[#368C64] text-sm mb-2">
+                            เติมเต็มช่องโหว่ทางการศึกษา
+                        </p>
+
+                        <h1 className="text-4xl font-bold leading-tight mb-4">
+                            เด็กทุกคนสมควรได้รับโอกาส <br />
+                            จากครูที่พร้อมเดินทางไปหา
+                        </h1>
+
+                        <p className="text-[#3F3F3F] mb-6">
+                            เชื่อมต่อคุณกับโรงเรียนที่ห่างไกลทั่วประเทศ ที่กำลังรอคอยครูอาสา ให้คุณได้ค้นหาโรงเรียนเหล่านั้นได้ด้วยตัวเอง ทำความเข้าใจเด็ก ๆ และบริบทของพื้นที่ เพื่อให้คุณตัดสินใจลงพื้นที่สอนได้อย่างตรงจุด
+                            และค้นพบโรงเรียนที่ใช่สำหรับคุณอย่างแท้จริง
+                        </p>
+
+                        <div className="flex gap-4">
+                        <button className="px-6 py-3 bg-[#368C64] text-white rounded-lg shadow hover:bg-green-700"
+                            onClick={handleFindSchool}>
+                            Fine a School →
+                        </button>
+
+                        <button className="px-6 py-3 border border-[#368C64] text-[#368C64] rounded-lg hover:bg-green-50"
+                            onClick={handlePostNeed}>
+                            Post a Need
+                        </button>
+                        </div>
+                    </div>
+
+                    <div className="bg-white p-4 rounded-xl shadow-md">
+                        <h2 className="text-center text-gray-600 mb-2">
+                            School Location
+                        </h2>
+
+                        <div className="h-[450px] rounded-lg overflow-hidden">
+                            <ThailandMap />
+                        </div>
+                    </div>
+
+                </div>
+        </div>
         </>
     )
 }
